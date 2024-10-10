9512d56569e802d9e0cfee06c30cc5e6c8caa092
Ricardo Costa <ricardocunhacosta@gmail.com>
2024-10-09 23:08:48 -0400

    criar tela de teste

--- src/controllers/bid/criar-valor-bid.controller.ts
+++ src/controllers/bid/criar-valor-bid.controller.ts
@@ -50,60 +50,60 @@ export class CriarValorBidController {
     ) {
         console.log('Dados recebidos no backend:', body); // Aqui você pode ver o corpo da requisição
 
-        const {
-            bidId,
-            seguroTransportadora,
-            seguroCompal,
-            frete
-        } = body;
+        // const {
+        //     bidId,
+        //     seguroTransportadora,
+        //     seguroCompal,
+        //     frete
+        // } = body;
 
-        // Verifica se o BID existe
-        const bidExists = await this.prisma.bid.findUnique({
-            where: { id: bidId },
-        });
-        if (!bidExists) {
-            throw new BadRequestException('BID não encontrado.');
-        }
+        // // Verifica se o BID existe
+        // const bidExists = await this.prisma.bid.findUnique({
+        //     where: { id: bidId },
+        // });
+        // if (!bidExists) {
+        //     throw new BadRequestException('BID não encontrado.');
+        // }
 
-        // Começa uma transação
-        const transaction = await this.prisma.$transaction(async (tx) => {
-            // Insere dados em SeguroTransportadora, se disponíveis
-            if (seguroTransportadora) {
-                await tx.seguroTransportadora.create({
-                    data: {
-                        descricao: seguroTransportadora.descricao,
-                        valor: seguroTransportadora.percentual,
-                        bidId: bidId,
-                    }
-                });
-            }
+        // // Começa uma transação
+        // const transaction = await this.prisma.$transaction(async (tx) => {
+        //     // Insere dados em SeguroTransportadora, se disponíveis
+        //     if (seguroTransportadora) {
+        //         await tx.seguroTransportadora.create({
+        //             data: {
+        //                 descricao: seguroTransportadora.descricao,
+        //                 valor: seguroTransportadora.percentual,
+        //                 bidId: bidId,
+        //             }
+        //         });
+        //     }
 
-            // Insere dados em ValorGeralDrr, se disponíveis
-            if (seguroCompal) {
-                await tx.valorGeralDrr.create({
-                    data: {
-                        descricao: seguroCompal.descricao,
-                        valor: seguroCompal.percentual,
-                        bidId: bidId,
-                    }
-                });
-            }
+        //     // Insere dados em ValorGeralDrr, se disponíveis
+        //     if (seguroCompal) {
+        //         await tx.valorGeralDrr.create({
+        //             data: {
+        //                 descricao: seguroCompal.descricao,
+        //                 valor: seguroCompal.percentual,
+        //                 bidId: bidId,
+        //             }
+        //         });
+        //     }
 
-            // Insere dados em Fretes, se disponíveis
-            if (frete) {
-                await tx.fretes.create({
-                    data: {
-                        descricao: frete.operacao,
-                        valorMinimo: frete.fretePesoMinimo,
-                        valorKg: frete.fretePesoMaximo,
-                        bidId: bidId,
-                    }
-                });
-            }
-        }).catch(err => {
-            // Lidar com erro da transação
-            throw new InternalServerErrorException('Erro ao salvar os dados.');
-        });
+        //     // Insere dados em Fretes, se disponíveis
+        //     if (frete) {
+        //         await tx.fretes.create({
+        //             data: {
+        //                 descricao: frete.operacao,
+        //                 valorMinimo: frete.fretePesoMinimo,
+        //                 valorKg: frete.fretePesoMaximo,
+        //                 bidId: bidId,
+        //             }
+        //         });
+        //     }
+        // }).catch(err => {
+        //     // Lidar com erro da transação
+        //     throw new InternalServerErrorException('Erro ao salvar os dados.');
+        // });
 
         return {
             message: 'Dados associados ao BID criados com sucesso.'
