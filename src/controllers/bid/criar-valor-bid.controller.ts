f313c27517c49597ab42f761fe0a35aa44168ad3
Ricardo Costa <ricardocunhacosta@gmail.com>
2024-10-09 23:10:19 -0400

    criar tela de teste

--- src/controllers/bid/criar-valor-bid.controller.ts
+++ src/controllers/bid/criar-valor-bid.controller.ts
@@ -15,26 +15,26 @@ import {
     PrismaService
 } from "src/prisma/prisma.service";
 
-// const criarValorBidBodySchema = z.object({
-//     bidId: z.string(),
-//     seguroTransportadora: z.object({
-//         descricao: z.string(),
-//         percentual: z.string()
-//     }).optional(),
-//     seguroCompal: z.object({
-//         descricao: z.string(),
-//         percentual: z.string()
-//     }).optional(),
-//     frete: z.object({
-//         fretePesoMinimo: z.string(),
-//         fretePesoMaximo: z.string(),
-//         operacao: z.string(),
-//     }).optional(),
-// });
+const criarValorBidBodySchema = z.object({
+    bidId: z.string(),
+    seguroTransportadora: z.object({
+        descricao: z.string(),
+        percentual: z.string()
+    }).optional(),
+    seguroCompal: z.object({
+        descricao: z.string(),
+        percentual: z.string()
+    }).optional(),
+    frete: z.object({
+        fretePesoMinimo: z.string(),
+        fretePesoMaximo: z.string(),
+        operacao: z.string(),
+    }).optional(),
+});
 
-// const bodyValidationPipe = new ZodValidationPipe(criarValorBidBodySchema);
+const bodyValidationPipe = new ZodValidationPipe(criarValorBidBodySchema);
 
-// type CriarValorBidBodySchema = z.infer<typeof criarValorBidBodySchema>;
+type CriarValorBidBodySchema = z.infer<typeof criarValorBidBodySchema>;
 
 @Controller('/valorbid')
 // @UseGuards(JwtAuthGuard)
@@ -45,65 +45,65 @@ export class CriarValorBidController {
     
     @Post()
     async handle(
-        @Body() body: any
+        @Body(bodyValidationPipe) body: CriarValorBidBodySchema
         
     ) {
         console.log('Dados recebidos no backend:', body); // Aqui você pode ver o corpo da requisição
 
-        // const {
-        //     bidId,
-        //     seguroTransportadora,
-        //     seguroCompal,
-        //     frete
-        // } = body;
+        const {
+            bidId,
+            seguroTransportadora,
+            seguroCompal,
+            frete
+        } = body;
 
-        // // Verifica se o BID existe
-        // const bidExists = await this.prisma.bid.findUnique({
-        //     where: { id: bidId },
-        // });
-        // if (!bidExists) {
-        //     throw new BadRequestException('BID não encontrado.');
-        // }
+        // Verifica se o BID existe
+        const bidExists = await this.prisma.bid.findUnique({
+            where: { id: bidId },
+        });
+        if (!bidExists) {
+            throw new BadRequestException('BID não encontrado.');
+        }
 
-        // // Começa uma transação
-        // const transaction = await this.prisma.$transaction(async (tx) => {
-        //     // Insere dados em SeguroTransportadora, se disponíveis
-        //     if (seguroTransportadora) {
-        //         await tx.seguroTransportadora.create({
-        //             data: {
-        //                 descricao: seguroTransportadora.descricao,
-        //                 valor: seguroTransportadora.percentual,
-        //                 bidId: bidId,
-        //             }
-        //         });
-        //     }
+        // Começa uma transação
+        const transaction = await this.prisma.$transaction(async (tx) => {
+            // Insere dados em SeguroTransportadora, se disponíveis
+            if (seguroTransportadora) {
+                await tx.seguroTransportadora.create({
+                    data: {
+                        descricao: seguroTransportadora.descricao,
+                        valor: seguroTransportadora.percentual,
+                        bidId: bidId,
+                    }
+                });
+            }
 
-        //     // Insere dados em ValorGeralDrr, se disponíveis
-        //     if (seguroCompal) {
-        //         await tx.valorGeralDrr.create({
-        //             data: {
-        //                 descricao: seguroCompal.descricao,
-        //                 valor: seguroCompal.percentual,
-        //                 bidId: bidId,
-        //             }
-        //         });
-        //     }
+            // Insere dados em ValorGeralDrr, se disponíveis
+            if (seguroCompal) {
+                await tx.valorGeralDrr.create({
+                    data: {
+                        descricao: seguroCompal.descricao,
+                        valor: seguroCompal.percentual,
+                        bidId: bidId,
+                    }
+                });
+            }
 
-        //     // Insere dados em Fretes, se disponíveis
-        //     if (frete) {
-        //         await tx.fretes.create({
-        //             data: {
-        //                 descricao: frete.operacao,
-        //                 valorMinimo: frete.fretePesoMinimo,
-        //                 valorKg: frete.fretePesoMaximo,
-        //                 bidId: bidId,
-        //             }
-        //         });
-        //     }
-        // }).catch(err => {
-        //     // Lidar com erro da transação
-        //     throw new InternalServerErrorException('Erro ao salvar os dados.');
-        // });
+            // Insere dados em Fretes, se disponíveis
+            if (frete) {
+                await tx.fretes.create({
+                    data: {
+                        descricao: frete.operacao,
+                        valorMinimo: frete.fretePesoMinimo,
+                        valorKg: frete.fretePesoMaximo,
+                        bidId: bidId,
+                    }
+                });
+            }
+        }).catch(err => {
+            // Lidar com erro da transação
+            throw new InternalServerErrorException('Erro ao salvar os dados.');
+        });
 
         return {
             message: 'Dados associados ao BID criados com sucesso.'
